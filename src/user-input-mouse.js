49769b98ce0eb56b28ec49f0dd5e150bdd9f98ef
Christopher Jacobs <apexearth@gmail.com>
2016-01-17 14:09:12 -0800

    functionality for handling mouse events and wheel events. x, y, button1,2,3, and wheel from deltaY

--- src/user-input-mouse.js
+++ src/user-input-mouse.js
@@ -12,13 +12,34 @@ function MouseInput(input) {
     var that = this;
     UserInput.call(this, input)
 
-    this.input.on('mousemove', handleMouseEvent)
-    this.input.on('mousedown', handleMouseEvent)
-    this.input.on('mouseup', handleMouseEvent)
+    this.input.on('mousemove', handleMouseEvent.bind(this, 'mousemove'))
+    this.input.on('mousedown', handleMouseEvent.bind(this, 'mousedown'))
+    this.input.on('mouseup', handleMouseEvent.bind(this, 'mouseup'))
+    this.input.on('wheel', handleWheelEvent);
 
-    function handleMouseEvent(e) {
-        that.values.x = e.clientX;
-        that.values.y = e.clientY;
+    function handleMouseEvent(name, e) {
+        that.x = e.clientX;
+        that.y = e.clientY;
+
+        if (e.button > 0) {
+            if (name == 'mousedown')
+                that["button" + e.button] = 1;
+            if (name == 'mouseup')
+                that["button" + e.button] = 0;
+        }
+    }
+
+    function handleWheelEvent(e) {
+        that.wheel = e.deltaY;
+    }
+
+    this.clear = clear
+    function clear() {
+        that.wheel = 0;
+        for (var key in that) {
+            if (key.substring(0, 6) == "button")
+                that[key] = 0
+        }
     }
 }
 util.inherits(MouseInput, UserInput)
--- test/user-input-mouse-Spec.js
+++ test/user-input-mouse-Spec.js
@@ -4,9 +4,60 @@ var mouseInput = require("../src/user-input-mouse.js")
 describe("user-input-mouse.js", function () {
 
     it("has default members", function () {
-        var input = keyboardInput()
+        var input = mouseInput()
         expect(input.values).toExist()
         expect(input.input).toExist()
+    })
+
+    it("reacts properly to mouse events", function () {
+        var mouse      = mouseInput()
+        var innerInput = mouse.input
+        innerInput.emit("mousemove", {
+            clientX: 3,
+            clientY: 4,
+            button:  0
+        })
+        expect(mouse.x).toEqual(3)
+        expect(mouse.y).toEqual(4)
+
+        innerInput.emit("mousedown", {
+            clientX: 7,
+            clientY: 8,
+            button:  1
+        })
+        expect(mouse.x).toEqual(7)
+        expect(mouse.y).toEqual(8)
+
+        expect(mouse.button1).toEqual(1)
+        innerInput.emit("mouseup", {
+            clientX: 3,
+            clientY: 4,
+            button:  1
+        })
+        expect(mouse.x).toEqual(3)
+        expect(mouse.y).toEqual(4)
+        expect(mouse.button1).toEqual(0)
+
+        innerInput.emit("wheel", {
+            deltaY: 1.5
+        })
+        expect(mouse.wheel).toEqual(1.5)
+
+        innerInput.emit("mousedown", {
+            clientX: 7,
+            clientY: 8,
+            button:  1
+        })
+        expect(mouse.x).toEqual(7)
+        expect(mouse.y).toEqual(8)
+        expect(mouse.wheel).toEqual(1.5)    // Shows value of last wheel event.
+
+        mouse.clear()                       // Clears wheel & button values.
+        expect(mouse.x).toEqual(7)
+        expect(mouse.y).toEqual(8)
+        expect(mouse.button1).toEqual(0)
+        expect(mouse.wheel).toEqual(0)
+
     })
 
 })
